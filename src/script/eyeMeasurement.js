f5e384100018b4fca41baf06e6c8682a11414620
Duncan <duncan.de.vlaminck@student.howest.be>
2020-06-12 14:17:02 +0200

    first commit

---
+++ src/script/eyeMeasurement.js
@@ -0,0 +1,24 @@
+import '../style/eyeMeasurement.scss';
+
+import navModule from './lib/navModule';
+import formModule from './lib/formModule';
+
+(() => {
+	document.addEventListener('DOMContentLoaded', () => {
+		const menuButtonElement = document.querySelector('.js-menu-button');
+		const menuCloseButtonElement = document.querySelector('.js-menu-close-button');
+		const menuElement = document.querySelector('.js-menu');
+
+		navModule.setup(menuButtonElement, menuCloseButtonElement, menuElement);
+
+		const formElement = document.querySelector('.js-form');
+		const inputElements = document.querySelectorAll('.js-input');
+		const labelElements = document.querySelectorAll('.js-label');
+
+		formModule.setup({
+			formElement,
+			inputElements,
+			labelElements,
+		});
+	});
+})();
